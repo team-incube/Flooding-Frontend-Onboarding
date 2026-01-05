8a1fd236de39471222f1e1a4d3706b88355c3869
koreahghg <koreahghg@gmail.com>
2026-01-05 18:18:21 +0900

    feat: 층수별 테이블 레이아웃 컴포넌트 추가

--- src/views/homebase/ui/HomebasePage/index.tsx
+++ src/views/homebase/ui/HomebasePage/index.tsx
@@ -2,6 +2,9 @@
 
 import Header from "@/widgets/header/ui";
 import { getTablesByFloor } from "@/features/homebase/lib/tableData";
+import Floor2Layout from "@/features/homebase/ui/Floor2";
+import Floor3Layout from "@/features/homebase/ui/Floor3";
+import Floor4Layout from "@/features/homebase/ui/Floor4";
 import { useState } from "react";
 
 export default function HomebasePage() {
@@ -73,231 +76,23 @@ export default function HomebasePage() {
           {selectedFloor && selectedClass ? (
             <>
               <div className="flex gap-3 h-full">
-                {selectedFloor === "2층" ? (
-                  <>
-                    <div className="flex-1 flex flex-col gap-4">
-                      <button
-                        onClick={() => setSelectedTable("Table 1")}
-                        className={`flex-1 p-5 rounded-xl border-1 text-left h-full flex flex-col justify-start cursor-pointer ${
-                          selectedTable === "Table 1"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 1" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 1
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 6명</p>
-                      </button>
-                    </div>
-                    <div className="w-8 bg-[#D1D8DE] rounded-lg"></div>
-                    <div className="flex-1 flex flex-col gap-4 h-full">
-                      <button
-                        onClick={() => setSelectedTable("Table 2")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer ${
-                          selectedTable === "Table 2"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#D1D8DE]"
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 2" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 2
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                      <button
-                        onClick={() => setSelectedTable("Table 3")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer ${
-                          selectedTable === "Table 3"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 3" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 3
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                    </div>
-                  </>
-                ) : selectedFloor === "3층" ? (
-                  <div className="flex flex-col gap-4 h-full flex-1">
-                    <div className="flex gap-3 flex-1 h-full">
-                      <button
-                        onClick={() => setSelectedTable("Table 1")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer ${
-                          selectedTable === "Table 1"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 1" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 1
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                      <div className="w-8 bg-[#D1D8DE] rounded-lg"></div>
-                      <button
-                        onClick={() => setSelectedTable("Table 2")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 2"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 2" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 2
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                    </div>
-                    <div className="flex gap-4 flex-1 h-full">
-                      <button
-                        onClick={() => setSelectedTable("Table 3")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 3"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9]"
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 3" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 3
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 6명</p>
-                      </button>
-                      <button
-                        onClick={() => setSelectedTable("Table 4")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 4"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 4" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 4
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                      <button
-                        onClick={() => setSelectedTable("Table 5")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 5"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 5" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 5
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 6명</p>
-                      </button>
-                    </div>
-                  </div>
-                ) : selectedFloor === "4층" ? (
-                  <div className="flex flex-col gap-4 h-full flex-1">
-                    <div className="flex gap-3 flex-1 h-full">
-                      <button
-                        onClick={() => setSelectedTable("Table 1")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer ${
-                          selectedTable === "Table 1"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9]"
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 1" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 1
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                      <div className="w-8 bg-[#D1D8DE] rounded-lg"></div>
-                      <button
-                        onClick={() => setSelectedTable("Table 2")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 2"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 2" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 2
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                    </div>
-                    <div className="flex gap-3 flex-1 h-full">
-                      <button
-                        onClick={() => setSelectedTable("Table 3")}
-                        className={`flex-[1.2] p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 3"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 3" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 3
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 6명</p>
-                      </button>
-                      <button
-                        onClick={() => setSelectedTable("Table 4")}
-                        className={`flex-1 p-5 rounded-xl border-1 flex flex-col items-start justify-start cursor-pointer cursor-pointer ${
-                          selectedTable === "Table 4"
-                            ? "border-main-100 bg-blue-50"
-                            : "border-[#E0E4E9] "
-                        }`}
-                      >
-                        <p
-                          className={`font-semibold mb-1 text-lg ${selectedTable === "Table 4" ? "text-gray-700" : "text-gray-700"}`}
-                        >
-                          Table 4
-                        </p>
-                        <p className="text-sm text-[#999999]">최대 4명</p>
-                      </button>
-                    </div>
-                  </div>
-                ) : (
-                  getTablesByFloor(selectedFloor).map((table) => (
-                    <button
-                      key={table.name}
-                      onClick={() => setSelectedTable(table.name)}
-                      className={`flex-1 p-4 rounded-lg border-1 flex flex-col items-start justify-start cursor-pointer ${
-                        selectedTable === table.name
-                          ? "border-main-100 bg-blue-50"
-                          : "border-[#E0E4E9] "
-                      }`}
-                    >
-                      <p
-                        className={`font-semibold mb-1 ${selectedTable === table.name ? "text-gray-700" : "text-gray-700"}`}
-                      >
-                        {table.name}
-                      </p>
-                      <p className="text-xs text-[#999999]">
-                        최대 {table.seats}명
-                      </p>
-                    </button>
-                  ))
+                {selectedFloor === "2층" && (
+                  <Floor2Layout
+                    selectedTable={selectedTable}
+                    setSelectedTable={setSelectedTable}
+                  />
+                )}
+                {selectedFloor === "3층" && (
+                  <Floor3Layout
+                    selectedTable={selectedTable}
+                    setSelectedTable={setSelectedTable}
+                  />
+                )}
+                {selectedFloor === "4층" && (
+                  <Floor4Layout
+                    selectedTable={selectedTable}
+                    setSelectedTable={setSelectedTable}
+                  />
                 )}
               </div>
             </>
