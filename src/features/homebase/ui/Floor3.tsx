aa335e1529f99f8d3dba9e97a5a382c1c5c76096
koreahghg <koreahghg@gmail.com>
2026-01-06 14:28:54 +0900

    feat: Floor 컴포넌트에 floor 및 classTime props 추가

--- src/features/homebase/ui/Floor3.tsx
+++ src/features/homebase/ui/Floor3.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import TableButton from "./TableButton";
 
 interface Floor3Props {
+  floor: string;
+  classTime: string;
   table: string | null;
   onTable: (table: string) => void;
 }
 
 export default function Floor3({
+  floor,
+  classTime,
   table,
   onTable,
 }: Floor3Props) {
@@ -17,6 +21,8 @@ export default function Floor3({
           name="Table 1"
           seats={4}
           isSelected={table === "Table 1"}
+          floor={floor}
+          classTime={classTime}
           onClick={() => onTable("Table 1")}
         />
         <div className="w-8 bg-[#D1D8DE] rounded-lg"></div>
@@ -24,6 +30,8 @@ export default function Floor3({
           name="Table 2"
           seats={4}
           isSelected={table === "Table 2"}
+          floor={floor}
+          classTime={classTime}
           onClick={() => onTable("Table 2")}
         />
       </div>
@@ -32,18 +40,24 @@ export default function Floor3({
           name="Table 3"
           seats={6}
           isSelected={table === "Table 3"}
+          floor={floor}
+          classTime={classTime}
           onClick={() => onTable("Table 3")}
         />
         <TableButton
           name="Table 4"
           seats={4}
           isSelected={table === "Table 4"}
+          floor={floor}
+          classTime={classTime}
           onClick={() => onTable("Table 4")}
         />
         <TableButton
           name="Table 5"
           seats={6}
           isSelected={table === "Table 5"}
+          floor={floor}
+          classTime={classTime}
           onClick={() => onTable("Table 5")}
         />
       </div>
