3d97445ada3ba09dbe416d320093974bd9c4a26e
moonkanghyeon <s25024@gsm.hs.kr>
2026-01-06 13:40:47 +0900

    entities/club export 업데이트

--- src/entities/club/index.ts
+++ src/entities/club/index.ts
@@ -1,2 +1,3 @@
-export { fetchClubs } from "./api/clubApi";
+export { fetchClubs, fetchClubById } from "./api/clubApi";
 export { useClubsQuery } from "./query/useClubsQuery";
+export { useClubQuery } from "./query/useClubQuery";
