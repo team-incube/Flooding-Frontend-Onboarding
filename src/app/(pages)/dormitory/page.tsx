3f56c0089ad842b2babb42fb1a03b72f44b592fd
ma94275 <s25020@gsm.hs.kr>
2026-01-08 16:11:08 +0900

    feat: 공용 상단바 적용

--- src/app/(pages)/dormitory/page.tsx
+++ src/app/(pages)/dormitory/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/widgets/header/ui/Navbar";
+import Header from "@/widgets/header/ui/index";
 import ChairSection from "@/widgets/chair/ui/ChairSection";
 import StudySection from "@/widgets/study/ui/StudySection";
 import DormitoryMusicSection from "@/features/dormitoryMusic/ui/DormitoryMusicSection";
@@ -6,8 +6,8 @@ import DormitoryMusicSection from "@/features/dormitoryMusic/ui/DormitoryMusicSe
 export default function DormitoryPage() {
     return (
         <div className="min-h-screen bg-[#F4F4F4]">
-            <Navbar/>
-            <div className="flex flex-col w-full pt-[174px] pb-[92px] px-4 gap-10">
+            <Header/>
+            <div className="flex flex-col w-full pt-[30px] pb-[92px] px-4 gap-10">
                 <StudySection/>
                 <ChairSection/>
                 <DormitoryMusicSection/>
